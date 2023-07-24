85110e80c176c5f1719c2313638435cd8d725c16
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-07-24 07:43:04 -0300

    finalizando item com render via server side

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,18 +17,69 @@ public class HomeController : Controller
 
     private MeuDbContext _context;
 
-    public IActionResult Index()
+    public IActionResult Index( string tipo )
     {
-        var lista = _context.Caixas.ToList();
+        List<Caixa> lista;
+        if(string.IsNullOrEmpty(tipo)){
+            lista = _context.Caixas.ToList();
+        }
+        else{
+            lista = _context.Caixas.Where(c => c.Tipo.ToLower().Contains(tipo.ToLower())).ToList();
+        }
+
         ViewBag.lista = lista;
-        
+
+        double valorTotal = 0;
+        double receitas = 0;
+        double despesas = 0;
+
+        foreach(var item in lista)
+        {
+            if(item.Status == Status.Entrada){
+                receitas += item.Valor;
+                valorTotal += item.Valor;
+            }
+            else{
+                despesas += item.Valor;
+                valorTotal -= item.Valor;
+            }
+        }
+
+
+        ViewBag.valorTotal = valorTotal;
+        ViewBag.receitas = receitas;
+        ViewBag.despesas = despesas;
+
         return View();
     }
 
-    public IActionResult Privacy()
+    [Route("/adicionar")]
+    public IActionResult adicionar()
     {
         return View();
     }
+
+    [Route("/adicionar-no-caixa")]
+    [HttpPost]
+    public IActionResult adicionarNoCaixa(Caixa caixa)
+    {
+        _context.Caixas.Add(caixa);
+        _context.SaveChanges();
+
+        return Redirect("/");
+    }
+
+    [Route("/excluir/{id}")]
+    [HttpGet]
+    public IActionResult excluir(int id)
+    {
+        var item = _context.Caixas.Find(id);
+        _context.Remove(item);
+        _context.SaveChanges();
+        
+        return Redirect("/");
+    }
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
